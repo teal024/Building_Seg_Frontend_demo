41a8321db733fdce0a9619a47f1aa9e660395f89
LuminousWanderer <1664524275@qq.com>
2023-11-28 15:17:45 +0800

    添加用户鉴权（前端思路）

---
+++ src/store/user.js
@@ -0,0 +1,29 @@
+// 管理用户数据
+
+import { defineStore } from "pinia"
+import { ref } from "vue"
+
+
+export const useUserStore = defineStore('user',() => {
+    const userInfo = ref({})
+    // 获取用户信息并存储
+    const getUserInfo = async({username,password}) => {
+        const token = '';
+        const res = await loginAPI({token, username,password})
+        userInfo.value = res.data
+        localStorage.setItem('jwt_token', token);
+    }
+
+    // 退出时清除用户信息
+    const clearUserInfo = ()=> {
+        userInfo.value={}
+    }
+
+    return {
+        userInfo,
+        getUserInfo,
+        clearUserInfo
+    }
+},{
+    persist:true,
+})
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,13 +1,33 @@
 <!-- 登录页面 -->
 <template>
     <div>本页面为登录页面</div>
-    <el-button @click="GoToLayout">点击跳转排版页面</el-button>
+    <el-button @click="GoToManagerLayout">点击跳转管理员排版页面</el-button>
+    <el-button @click="GoToUserLayout">点击跳转普通用户排版页面</el-button>
 </template>
 
 <script setup>
 import router from "@/router/index.js"
+import { useUserStore } from '@/store/user';
+import { onMounted } from 'vue'
 
-const GoToLayout = () => {
+const userStore = useUserStore();
+
+onMounted(() => {
+    userStore.clearUserInfo()
+});
+
+const GoToManagerLayout = () => {
+    userStore.userInfo.role = 'Manager'
+    router.push({ 
+        name: 'layout', 
+        params:{ 
+            choice:'dashboard' 
+        } 
+    })
+}
+
+const GoToUserLayout = () => {
+    userStore.userInfo.role = 'User'
     router.push({ 
         name: 'layout', 
         params:{ 
